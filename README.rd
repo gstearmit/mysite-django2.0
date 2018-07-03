9da120dc6d2931e589b09679b27cd73d2b1ac0a6
phuchc <phuchc@peacesoft.net>
2018-07-03 16:06:09 +0700

    Add Modul Debug Tools Bar For Django 2.0

--- README.rd
+++ README.rd
@@ -2,3 +2,7 @@ cd /Users/gstearmit/mysite
 virtualenv -p python env
 env/bin/pip install django
 source env/bin/activate
+
+
+---------------------
+python manage.py runserver 8080
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path('', include('polls.urls')),  # index
     path('polls/', include('polls.urls')),  # Route Polls
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+   import debug_toolbar
+   urlpatterns += [
+       path('debug/', include(debug_toolbar.urls)),
+   ]
